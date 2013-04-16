ea78f7a175b2a29fcae1f6badfe464100c0c80e2
Jared Knowles <jknowles@gmail.com>
2013-04-16 16:41:53 -0500

    update

--- workflow.R
+++ workflow.R
@@ -27,7 +27,7 @@ install_pkgs<-function(mypkgs,newlib){
   install.packages(mypkgs[,1],lib=newlib)
 }
 
-install_pkgs(a,newLib)
+installPkgs(a,newLib)
 
 # Remove old libs
 
@@ -35,7 +35,7 @@ add_new_lib<-function(newlib){
   .libPaths(c(.libPaths(),newlib))
 }
 
-add_new_lib(newLib)
+addNewLib(newLib)
 
 Sys.setenv("R_LIBS_SITE" = newLib )
 
@@ -44,29 +44,7 @@ identical(Sys.getenv("R_LIBS_SITE"),newLib)
 # http://www.r-statistics.com/wp-content/uploads/2010/04/upgrading-R-on-windows.r.txt
 # http://stackoverflow.com/questions/1401904/painless-way-to-install-a-new-version-of-r-on-windows
 
-set_library<-function(){
-  if(grepl("/", R.home(), fixed = T))
-  { R_parent_lib <- paste(head(strsplit(R.home(), "/", fixed = T)[[1]], -1), collapse = "/") }
-  if(grepl("\\", R.home(), fixed = T))
-  { R_parent_lib <- paste(head(strsplit(R.home(), "\\", fixed = T)[[1]], -1), collapse = "/") }
-  # if global.library.folder isn't defined, then we assume it is of the form: "C:\\Program Files\\R\\library"
-  #R_parent_lib <- paste(head(strsplit(R.home(), "/", fixed = T)[[1]], -1), collapse = "/")
-  global.library.folder <- paste(R_parent_lib, "/lib", sep = "")
+setLibrary<-function()
 
-  Renviron.site.loc <- paste(R.home(), "\\etc\\Renviron.site", sep = "")
-  if(!file.exists(Renviron.site.loc))
-    {	# If "Renviron.site" doesn't exist (which it shouldn't be) - create it and add the global lib line to it.
-    cat(paste("R_LIBS='",global.library.folder, "'\n",sep = "") ,
-      file = Renviron.site.loc)				 
-    cat(paste("The file:" , Renviron.site.loc, "didn't exist - we created it and added your 'Global library link' (",global.library.folder,") to it.\n"))
-} else {
-  cat(paste("The file:" , Renviron.site.loc, "existed and we could NOT add some lines!  make sure you add the following line by yourself:","\n"))
-  cat(paste("R_LIBS=",global.library.folder,"\n", sep = "") )
-  cat(paste("To the file:",Renviron.site.loc,"\n"))
-}
-}
-
-.Library.site
-.Library.site <- file.path(chartr("\\", "/", R.home()), "site-library")
 
 
