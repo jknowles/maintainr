3f5cbcd1af7db6a959f8e9836540360adef748ae
Jared Knowles <jknowles@gmail.com>
2016-12-21 14:26:13 -0500

    doco cleanup

--- R/cloudUploaders.R
+++ R/cloudUploaders.R
@@ -2,16 +2,16 @@
 
 #' Backup R package list
 #'
-#' @param backupPath The path to where \code{\link{savePkgs}} should write the 
+#' @param backupPath The path to where \code{\link{savePkgs}} should write the
 #' list of packages, optional
 #' @param filename The name of the csv file to save the package names, optional
-#' @param cloudProvider Optional, if specified the package will attempt to write 
+#' @param cloudProvider Optional, if specified the package will attempt to write
 #' the package list to a tempfile and then upload that tempfile to a cloud service
 #' @param ... Additional arguments to pass to \code{\link{writeCloud}}
 #' @return Nothing
 #' @export
 #'
-pkg_backup <- function(backupPath = NULL, filename = NULL, 
+pkg_backup <- function(backupPath = NULL, filename = NULL,
                        cloudProvider = NULL, ...){
   if(missing(backupPath)){
     backupPath <- tempdir()
@@ -33,10 +33,10 @@ pkg_backup <- function(backupPath = NULL, filename = NULL,
 #' Restore R packages from a package list stored somewhere else
 #'
 #' @param install logical, should packages in the package list be installed?
-#' @param backupPath The path to where \code{\link{savePkgs}} should write the 
+#' @param backupPath The path to where \code{\link{savePkgs}} should write the
 #' list of packages, optional
 #' @param filename The name of the csv file to save the package names, optional
-#' @param cloudProvider Optional, if specified the package will attempt to write 
+#' @param cloudProvider Optional, if specified the package will attempt to write
 #' the package list to a tempfile and then upload that tempfile to a cloud service
 #' @param libpath Path to the library
 #' @param update A logical, should packages be updated first? Default is false
@@ -45,17 +45,17 @@ pkg_backup <- function(backupPath = NULL, filename = NULL,
 #'
 #' @return Nothing.
 #' @export
-pkg_restore <- function(install = TRUE, backupPath = NULL, filename = NULL, 
-                        cloudProvider = NULL, libpath = NULL, update = FALSE, 
+pkg_restore <- function(install = TRUE, backupPath = NULL, filename = NULL,
+                        cloudProvider = NULL, libpath = NULL, update = FALSE,
                         keep_all = FALSE, ...){
   if(!missing(cloudProvider)){
     pkgList <- readCloud(filename = filename, provider = cloudProvider, ...)
   } else {
-    pkgList <- read_pkgs(backupPath, filename=filename)
+    pkgList <- readPkgs(backupPath, filename=filename)
   }
   if(install){
     # First validate
-    installPkgs(pkgList, libpath = libpath, update = update, 
+    installPkgs(pkgList, libpath = libpath, update = update,
                 keep_all = keep_all)
   } else{
     pkgList <- sync_pkgs(pkgList = pkgList, keep_all = keep_all)
@@ -71,15 +71,15 @@ pkg_restore <- function(install = TRUE, backupPath = NULL, filename = NULL,
 #' Cloud upload handler
 #'
 #' @param filename path and filename of file to be uploaded to cloud storage
-#' @param provider a character for the provider to use, currently only "dropbox" 
+#' @param provider a character for the provider to use, currently only "dropbox"
 #' is supported
 #' @param ... additional arguments to pass to the function that uploads the file
 #'
-#' @return Nothing. 
-##' @seealso \pkg{\link{rdrop2}}
+#' @return Nothing.
+#' @seealso \code{\link[rdrop2:drop_read_csv]{drop_upload}}
 #' @export
 writeCloud <- function(filename, provider = c("dropbox"), ...){
-  provider <- match.arg(provider, 
+  provider <- match.arg(provider,
                         choices = c("dropbox"), several.ok = FALSE)
   if(provider == "dropbox"){
     requireNamespace("rdrop2", quietly = TRUE)
@@ -90,15 +90,15 @@ writeCloud <- function(filename, provider = c("dropbox"), ...){
 #' Cloud fetch handler
 #'
 #' @param filename path to file to be uploaded to cloud storage
-#' @param provider a character for the provider to use, currently only "dropbox" 
+#' @param provider a character for the provider to use, currently only "dropbox"
 #' is supported
 #' @param ... additional arguments to pass to the function that uploads the file
 #'
 #' @return Nothing.
-##' @seealso \pkg{\link{rdrop2}}
+#' @seealso \code{\link[rdrop2:drop_read_csv]{drop_upload}}
 #' @export
 readCloud <- function(filename, provider = c("dropbox"), ...){
-  provider <- match.arg(provider, 
+  provider <- match.arg(provider,
                         choices = c("dropbox"), several.ok = FALSE)
   if(provider == "dropbox"){
     requireNamespace("rdrop2", quietly = TRUE)
---
+++ R/maintainr-package.r
@@ -0,0 +1,24 @@
+#' maintainr: Provides a simple method for saving and restoring a list of R
+#' packages installed on a machine.
+#'
+#' maintainr is not intended to be a replacement for more comprehensive archiving
+#' packages such as packrat or MRAN, but instead is just a simple way to save
+#' a list of installed packages to a central location and read that list into
+#' a new R install and optionally install the packages.
+#'
+#' @name maintainr
+#' @docType package
+#' @aliases maintainr maintainr-package
+NULL
+
+
+
+# Global variables
+# utils::globalVariables(c(".shinyMerPar", "sig", "sigma", "Lind", "group"))
+
+#' @importFrom utils head install.packages installed.packages read.csv update.packages
+#' write.csv
+zzz <- function(){
+  # Nothing
+
+}
